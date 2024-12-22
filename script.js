4582c377141fd843346135c9e25b472324a9b84d
sahiltcg <sahilahmedtcg@gmail.com>
2024-12-22 12:16:40 +0530

    Script.js updated

---
+++ script.js
@@ -0,0 +1,13 @@
+// Array Fill Method
+
+// let arr1 = [1,2,3,4,5,6,7];
+// arr1.fill("sahil", 2,4);
+// console.log(arr1);
+
+// Array Concatination Method
+
+let arr1 = [1,2,3,4];
+let arr2 = [5,6,7,8];
+let arr3 = [12,3,4,23,454];
+
+console.log(arr1.concat(arr2, arr3));
