6c095628006f5fc9e5c198bacabcf6cdd95bcf10
sahiltcg <sahilahmedtcg@gmail.com>
2024-12-24 16:29:48 +0530

    Updated

--- JS practice/script1.js
+++ JS practice/script1.js
@@ -2,7 +2,23 @@
 
 // Array.from
 
-let a = "sahil";
-console.log(Array.from(a));
+// let a = "sahil";
+// console.log(Array.from(a));
 // => It creates array from an array like objects.
 
+// Array.isArray
+
+// let a = ["sahil", "raju", "jethalal", "spiderman", "panda"];
+// let b = "I'm coding";
+
+// if(Array.isArray(a) == true){
+//     console.log(`The element "a" is an array`);
+// }else{
+//     console.log(`The element "a" is not array`)
+// }
+
+// if(Array.isArray(b) == true){
+//     console.log(`The element "b" is an array`);
+// }else{
+//     console.log(`The element "b" is not array`)
+// }
