d834e6e8f5fe71f674e3e8ff449afe903a228115
sahiltcg <sahilahmedtcg@gmail.com>
2024-12-27 16:07:59 +0530

    Functions in JS

---
+++ 01-Basics/JavaScript/fuctions.js
@@ -0,0 +1,24 @@
+// Let's study functions in Javascript...
+
+// => Functions help us repeat the same block of code multiple times.
+
+// Syntax =>
+
+    // function name(params) {
+        
+    // }
+
+
+
+function sum(num1, num2){
+//    let num1;
+//    let num2;
+
+    console.log("The total is: ", num1 + num2);
+}
+
+sum(12, 23);
+sum(42, 21);
+sum(45, 28);
+sum(3245, 2128);
+sum(4512, 232);
