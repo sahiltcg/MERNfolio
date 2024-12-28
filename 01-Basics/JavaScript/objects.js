a297fe22bc9c2e4c28c484f40b6da2f3cb23fc13
sahiltcg <sahilahmedtcg@gmail.com>
2024-12-28 16:15:43 +0530

    updated

--- 01-Basics/JavaScript/objects.js
+++ 01-Basics/JavaScript/objects.js
@@ -17,20 +17,53 @@
 
 // Exercise 1 - Basic (Nested Objects)
 
-let student = {
-    name: 'Sahil',
-    grade: 12,
-    address: {
-        street: 'Gulshan Nagar',
-        city: 'bhilwara',
-        zipCode: 311001
-    }
-}
+// let student = {
+//     name: 'Sahil',
+//     grade: 12,
+//     address: {
+//         street: 'Gulshan Nagar',
+//         city: 'bhilwara',
+//         zipCode: 311001
+//     }
+// }
 
-let cityname = student.address.city
+// let cityname = student.address.city
 
-console.log(cityname)
+// console.log(cityname)
 
-let stuname = student["name"]
+// let stuname = student["name"]
 
-console.log(stuname)
+// console.log(stuname)
+
+
+// Exercise 2 - Intermediate 
+
+// Iterating Through an Object
+
+// let student = {
+//     name: 'Sahil',
+//     grade: 12,
+//     address: {
+//         street: 'Gulshan Nagar',
+//         city: 'bhilwara',
+//         zipCode: 311001
+//     }
+// }
+
+// for (let x in student){
+//     console.log(x + ": " + student[x])
+// }
+
+
+// Dynamic Keys
+
+// => Create an empty object book.
+// => Dynamically add properties like title, author, and yearPublished using variables.
+
+const book = new Object()
+
+book.title = "Atomic Habits"
+book.author = "Sahil"
+book.yearPublished = 2020
+
+console.log(book)
