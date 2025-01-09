861b75e3ab87218cd049b82c83231f464e968227
sahiltcg <sahilahmedtcg@gmail.com>
2025-01-09 11:07:04 +0530

    revision

---
+++ 01-Basics/JavaScript/revision.js
@@ -0,0 +1,18 @@
+let sahil = {
+    name: "Sahil",
+    age: 22,
+    city: "Delhi",
+    courseName: [],
+    addCourse: function(courseName) {
+        this.courseName.push(courseName);
+    },
+    getCourseCount: function() {
+        return `${this.name} is enrolled in total of ${this.courseName.length} courses`;
+    }
+}
+
+sahil.addCourse("JavaScript");
+sahil.addCourse("React");
+sahil.addCourse("NodeJS");
+console.log(sahil.getCourseCount());
+// console.log(sahil);
