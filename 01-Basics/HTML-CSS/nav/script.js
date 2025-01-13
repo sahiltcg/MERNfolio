f650af03bfd42e0711f28503aeed2378b16f0915
sahiltcg <sahilahmedtcg@gmail.com>
2025-01-13 15:42:08 +0530

    updated

---
+++ 01-Basics/HTML-CSS/nav/script.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    let sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "flex"
+}
+
+function hideSidebar(){
+    let sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "none"
+}
