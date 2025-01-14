a1ccaa419c015b68d6202049b308b205283deb86
sahiltcg <sahilahmedtcg@gmail.com>
2025-01-14 11:24:48 +0530

    Object is JS

---
+++ Javascript/s9.js
@@ -0,0 +1,13 @@
+// Objects in Javascript
+
+let car = {
+    make: "Tesla",
+    model: "Falcon",
+    year: 2024,
+    start: function(){       //This is a method inside an object
+        return "Car started"
+    }
+}
+
+console.log(car.start());
+
