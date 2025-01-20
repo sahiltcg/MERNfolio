271249dbc023c644dc498f510895699f82175bd5
sahiltcg <sahilahmedtcg@gmail.com>
2025-01-20 12:47:56 +0530

    Updated

--- practice/array.js
+++ practice/array.js
@@ -14,6 +14,18 @@ const lenght = arr.filter((item)=> item.length >= 5)
 // Checkig if the number is even or not using Array filter method
 let num = [1,2,3,4,5,6,7,8,9]
 let evenNumbers = num.filter((item)=> item % 2 === 0)
-console.log(evenNumbers);
+// console.log(evenNumbers);
 
 
+// Filtering by Object Property
+
+const users = [
+    { name: 'Alice', age: 25 },
+    { name: 'Bob', age: 30 },
+    { name: 'Charlie', age: 25 },
+    { name: 'John', age: 45 },
+    { name: 'Sam', age: 54 },
+];
+
+const userfilter = users.filter((user)=> user.age > 40)
+console.log(userfilter);
