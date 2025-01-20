31b80d244db77900c7a41e49e01a0b57080e746a
sahiltcg <sahilahmedtcg@gmail.com>
2025-01-20 12:43:24 +0530

    Array practice

--- practice/array.js
+++ practice/array.js
@@ -8,4 +8,12 @@ const inc = arr.filter((item)=> item.includes("s"));
 
 // To check, if the array contens any word greate then the given lenght
 const lenght = arr.filter((item)=> item.length >= 5)
-console.log(lenght);
+// console.log(lenght);
+
+
+// Checkig if the number is even or not using Array filter method
+let num = [1,2,3,4,5,6,7,8,9]
+let evenNumbers = num.filter((item)=> item % 2 === 0)
+console.log(evenNumbers);
+
+
